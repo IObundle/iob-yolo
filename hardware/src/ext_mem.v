1c7c3fd445bc14fd30db09e6a5c1a953c9120f75
P_Miranda <pedro_miranda2@hotmail.com>
2020-08-11 18:26:55 +0100

    splited read and write databuses from versat

--- hardware/src/ext_mem.v
+++ hardware/src/ext_mem.v
@@ -18,6 +18,8 @@
 `define wstrb_MIG_BUS(I) I*`REQ_MIG_BUS_W +: (`MIG_BUS_W/8)
 `define rdata_MIG_BUS(I) I*`RESP_MIG_BUS_W+`RDATA_P +: `MIG_BUS_W
 `define ready_MIG_BUS(I) I*`RESP_MIG_BUS_W
+`define req_MIG_BUS(I) I*`REQ_MIG_BUS_W +: `REQ_MIG_BUS_W
+`define resp_MIG_BUS(I) I*`RESP_MIG_BUS_W +: `RESP_MIG_BUS_W
 
 // REQ/RESP VERSAT BUS DEFINES
 `ifdef USE_NEW_VERSAT
@@ -31,6 +33,8 @@
   `define wstrb_VERSAT(I) I*`REQ_VERSAT_W +: (`DATAPATH_W/8)
   `define rdata_VERSAT(I) I*`RESP_VERSAT_W+`RDATA_P +: `DATAPATH_W
   `define ready_VERSAT(I) I*`RESP_VERSAT_W
+  `define req_VERSAT(I) I*`REQ_VERSAT_W +: `REQ_VERSAT_W
+  `define resp_VERSAT(I) I*`RESP_VERSAT_W +: `RESP_VERSAT_W
 `endif
 
 module ext_mem
@@ -208,17 +212,46 @@ module ext_mem
    // VERSAT L1 Caches
    //
 
-   //Back-end bus
-   wire [3*`REQ_MIG_BUS_W-1:0] 	  vcache_be_req;
-   wire [3*`RESP_MIG_BUS_W-1:0]	  vcache_be_resp;
+   //Front-end bus
+   wire [`REQ_VERSAT_W-1:0] 	  vread_fe_req;
+   wire [`RESP_VERSAT_W-1:0] 	  vread_fe_resp;
    
-   genvar			  l;
+   //Back-end bus
+   wire [2*`REQ_MIG_BUS_W-1:0] 	  vcache_be_req;
+   wire [2*`RESP_MIG_BUS_W-1:0]	  vcache_be_resp;
+
+   // Merge VRead databuses
+   wire [2*`REQ_VERSAT_W-1:0] 	  m_vread_req;
+   wire [2*`RESP_VERSAT_W-1:0]   m_vread_resp;
+
+   // native to interconnect interface
+   genvar 			  k;
    generate
-      for(l = 0; l < 3; l++) begin : vcaches_l1
-
-	 wire [`IO_ADDR_W-1:0] vcache_addr = databus_addr[3*`IO_ADDR_W-l*`IO_ADDR_W-1 -: `IO_ADDR_W];
-
-         // Versat cache instance
+      for(k=0;k<2;k=k+1) begin : databus_to_native
+	 assign m_vread_req[`valid_VERSAT(k)] = databus_valid[k];
+	 assign m_vread_req[`address_VERSAT(k, `IO_ADDR_W)] = databus_addr[k*`IO_ADDR_W +: `IO_ADDR_W];
+	 assign m_vread_req[`wdata_VERSAT(k)] = databus_wdata[k*`DATAPATH_W +: `DATAPATH_W];
+	 assign m_vread_req[`wstrb_VERSAT(k)] = databus_wstrb[k*`DATAPATH_W/8 +: `DATAPATH_W/8];
+	 assign databus_rdata[k*`DATAPATH_W +: `DATAPATH_W] = m_vread_resp[`rdata_VERSAT(k)];
+	 assign databus_ready[k] = m_vread_resp[`ready_VERSAT(k)];	 
+      end
+   endgenerate
+   
+   // vread merge
+   merge #(
+	   .N_MASTERS(2),
+	   .DATA_W(`DATAPATH_W),
+	   .ADDR_W(`IO_ADDR_W)
+	   ) vread_merge (
+			  //masters interface
+			  .m_req(m_vread_req),
+			  .m_resp(m_vread_resp),
+			  //slave interface
+			  .s_req(vread_fe_req),
+			  .s_resp(vread_fe_resp)
+			  );
+   
+         // VRead cache instance
     	 iob_cache # (
            .FE_ADDR_W(`DDR_ADDR_W),
            .N_WAYS(1),        //Number of ways
@@ -228,40 +261,130 @@ module ext_mem
            .CTRL_CNT(1),       //Counters for hits and misses (since previous parameter is 0)
            .FE_DATA_W(`DATAPATH_W), //DATAPATH_W = 16 front-end
            .BE_DATA_W(`MIG_BUS_W)
-        ) vcache (
+        ) vcache_vread (
      	   .clk   (clk),
     	   .reset (rst),
 
     	   // Front-end interface
-   	   .valid (databus_valid[2-l -: 1]),
-   	   .addr  (vcache_addr[`DDR_ADDR_W:1]),
-   	   .wdata (databus_wdata[3*`DATAPATH_W-l*`DATAPATH_W-1 -: `DATAPATH_W]),
-  	   .wstrb (databus_wstrb[3*`DATAPATH_W/8-l*`DATAPATH_W/8-1 -: `DATAPATH_W/8]),
-   	   .rdata (databus_rdata[3*`DATAPATH_W-l*`DATAPATH_W-1 -: `DATAPATH_W]),
-   	   .ready (databus_ready[2-l -: 1]),
+   	   .valid (vread_fe_req[`valid_VERSAT(0)]),
+   	   .addr  (vread_fe_req[`address_VERSAT(0,`DDR_ADDR_W+1)-1]),
+   	   .wdata (vread_fe_req[`wdata_VERSAT(0)]),
+  	   .wstrb (vread_fe_req[`wstrb_VERSAT(0)]),
+   	   .rdata (vread_fe_resp[`rdata_VERSAT(0)]),
+   	   .ready (vread_fe_resp[`ready_VERSAT(0)]),
 		 
     	    // Back-end interface
-    	   .mem_valid (vcache_be_req[`valid_MIG_BUS(l)]),
-    	   .mem_addr  (vcache_be_req[`address_MIG_BUS(l, `DDR_ADDR_W)]),
-    	   .mem_wdata (vcache_be_req[`wdata_MIG_BUS(l)]),
-    	   .mem_wstrb (vcache_be_req[`wstrb_MIG_BUS(l)]),
-    	   .mem_rdata (vcache_be_resp[`rdata_MIG_BUS(l)]),
-    	   .mem_ready (vcache_be_resp[`ready_MIG_BUS(l)])
+    	   .mem_valid (vcache_be_req[`valid_MIG_BUS(0)]),
+    	   .mem_addr  (vcache_be_req[`address_MIG_BUS(0, `DDR_ADDR_W)]),
+    	   .mem_wdata (vcache_be_req[`wdata_MIG_BUS(0)]),
+    	   .mem_wstrb (vcache_be_req[`wstrb_MIG_BUS(0)]),
+    	   .mem_rdata (vcache_be_resp[`rdata_MIG_BUS(0)]),
+    	   .mem_ready (vcache_be_resp[`ready_MIG_BUS(0)])
         );
 
         //assign MSBs of address fields
-   	assign vcache_be_req[`address_MIG_BUS(l, `ADDR_W)-`DDR_ADDR_W] = 0;
+   	assign vcache_be_req[`address_MIG_BUS(0, `ADDR_W)-`DDR_ADDR_W] = 0;
+
+   
+         // VWrite cache instance
+    	 iob_cache # (
+           .FE_ADDR_W(`DDR_ADDR_W),
+           .N_WAYS(1),        //Number of ways
+           .LINE_OFF_W(1),    //Cache Line Offset (number of lines)
+           .WORD_OFF_W(5),    //Word Offset (number of words per line)
+           .WTBUF_DEPTH_W(4), //FIFO's depth
+           .CTRL_CNT(1),       //Counters for hits and misses (since previous parameter is 0)
+           .FE_DATA_W(`DATAPATH_W), //DATAPATH_W = 16 front-end
+           .BE_DATA_W(`MIG_BUS_W)
+        ) vcache_vwrite (
+     	   .clk   (clk),
+    	   .reset (rst),
+
+    	   // Front-end interface
+   	   .valid (databus_valid[2]),
+   	   .addr  (databus_addr[2*`IO_ADDR_W+1 +: `DDR_ADDR_W]),
+   	   .wdata (databus_wdata[3*`DATAPATH_W-1 -: `DATAPATH_W]),
+  	   .wstrb (databus_wstrb[3*`DATAPATH_W/8-1 -: `DATAPATH_W/8]),
+   	   .rdata (databus_rdata[3*`DATAPATH_W-1 -: `DATAPATH_W]),
+   	   .ready (databus_ready[2]),
+		 
+    	    // Back-end interface
+    	   .mem_valid (vcache_be_req[`valid_MIG_BUS(1)]),
+    	   .mem_addr  (vcache_be_req[`address_MIG_BUS(1, `DDR_ADDR_W)]),
+    	   .mem_wdata (vcache_be_req[`wdata_MIG_BUS(1)]),
+    	   .mem_wstrb (vcache_be_req[`wstrb_MIG_BUS(1)]),
+    	   .mem_rdata (vcache_be_resp[`rdata_MIG_BUS(1)]),
+    	   .mem_ready (vcache_be_resp[`ready_MIG_BUS(1)])
+        );
+
+        //assign MSBs of address fields
+   	assign vcache_be_req[`address_MIG_BUS(1, `ADDR_W)-`DDR_ADDR_W] = 0;
+
+   
+
+   
+   // genvar			  l;
+   // generate
+   //    for(l = 0; l < 3; l++) begin : vcaches_l1
+
+   // 	 wire [`IO_ADDR_W-1:0] vcache_addr = databus_addr[3*`IO_ADDR_W-l*`IO_ADDR_W-1 -: `IO_ADDR_W];
+
+   //       // Versat cache instance
+   //  	 iob_cache # (
+   //         .FE_ADDR_W(`DDR_ADDR_W),
+   //         .N_WAYS(1),        //Number of ways
+   //         .LINE_OFF_W(1),    //Cache Line Offset (number of lines)
+   //         .WORD_OFF_W(5),    //Word Offset (number of words per line)
+   //         .WTBUF_DEPTH_W(4), //FIFO's depth
+   //         .CTRL_CNT(1),       //Counters for hits and misses (since previous parameter is 0)
+   //         .FE_DATA_W(`DATAPATH_W), //DATAPATH_W = 16 front-end
+   //         .BE_DATA_W(`MIG_BUS_W)
+   //      ) vcache (
+   //   	   .clk   (clk),
+   //  	   .reset (rst),
+
+   //  	   // Front-end interface
+   // 	   .valid (databus_valid[2-l -: 1]),
+   // 	   .addr  (vcache_addr[`DDR_ADDR_W:1]),
+   // 	   .wdata (databus_wdata[3*`DATAPATH_W-l*`DATAPATH_W-1 -: `DATAPATH_W]),
+   // 	   .wstrb (databus_wstrb[3*`DATAPATH_W/8-l*`DATAPATH_W/8-1 -: `DATAPATH_W/8]),
+   // 	   .rdata (databus_rdata[3*`DATAPATH_W-l*`DATAPATH_W-1 -: `DATAPATH_W]),
+   // 	   .ready (databus_ready[2-l -: 1]),
+		 
+   //  	    // Back-end interface
+   //  	   .mem_valid (vcache_be_req[`valid_MIG_BUS(l)]),
+   //  	   .mem_addr  (vcache_be_req[`address_MIG_BUS(l, `DDR_ADDR_W)]),
+   //  	   .mem_wdata (vcache_be_req[`wdata_MIG_BUS(l)]),
+   //  	   .mem_wstrb (vcache_be_req[`wstrb_MIG_BUS(l)]),
+   //  	   .mem_rdata (vcache_be_resp[`rdata_MIG_BUS(l)]),
+   //  	   .mem_ready (vcache_be_resp[`ready_MIG_BUS(l)])
+   //      );
+
+   //      //assign MSBs of address fields
+   // 	assign vcache_be_req[`address_MIG_BUS(l, `ADDR_W)-`DDR_ADDR_W] = 0;
 	 	 
-      end
-   endgenerate
+   //    end
+   // endgenerate
 `endif
    
    // Merge caches back-ends
-   wire [`REQ_MIG_BUS_W-1:0]      l2cache_req;
-   wire [`RESP_MIG_BUS_W-1:0]     l2cache_resp;
+   wire [2*`REQ_MIG_BUS_W-1:0]      l2cache_req;
+   wire [2*`RESP_MIG_BUS_W-1:0]     l2cache_resp;
 
    wire 			  l2cache_read_valid, l2cache_read_ready;
    wire 			  l2cache_write_valid, l2cache_write_ready;
+   
+   wire [`REQ_MIG_BUS_W-1:0] 	  dcache_read_be_req, dcache_write_be_req;
+   wire [`RESP_MIG_BUS_W-1:0] 	  dcache_read_be_resp, dcache_write_be_resp;
+
+   assign dcache_read_be_req[`valid_MIG_BUS(0)] = dcache_be_req[`valid_MIG_BUS(0)] && (~(|dcache_be_req[`wstrb_MIG_BUS(0)]));
+   assign dcache_read_be_req[`REQ_MIG_BUS_W-2:0] = dcache_be_req[`REQ_MIG_BUS_W-2:0];
+
+   assign dcache_write_be_req[`valid_MIG_BUS(0)] = dcache_be_req[`valid_MIG_BUS(0)] && (|dcache_be_req[`wstrb_MIG_BUS(0)]);
+   assign dcache_write_be_req[`REQ_MIG_BUS_W-2:0] = dcache_be_req[`REQ_MIG_BUS_W-2:0];
+   
+   assign dcache_be_resp[`ready_MIG_BUS(0)] = dcache_read_be_resp[`ready_MIG_BUS(0)] | dcache_write_be_resp[`ready_MIG_BUS(0)];
+   assign dcache_be_resp[`rdata_MIG_BUS(0)] = dcache_read_be_resp[`rdata_MIG_BUS(0)];
    
    
 
@@ -270,55 +393,114 @@ module ext_mem
 `endif
    assign dcache_be_req[`address_MIG_BUS(0,`ADDR_W)-`DDR_ADDR_W] = 0;
    
+//    merge
+//      #(
+// `ifdef RUN_DDR_USE_SRAM
+//      .N_MASTERS(2),
+// `else
+//  `ifdef USE_NEW_VERSAT
+//        .N_MASTERS(1+1),
+//  `else
+//        .N_MASTERS(1),
+//  `endif
+// `endif
+//        .DATA_W(`MIG_BUS_W)
+//        )
+//      merge_i_d_buses_into_l2_read
+//        (
+//         // masters
+// `ifdef RUN_DDR_USE_SRAM
+//         .m_req  ({icache_be_req, dcache_be_req}),
+//         .m_resp ({icache_be_resp, dcache_be_resp}),
+// `else
+//  `ifdef USE_NEW_VERSAT
+// 	.m_req  ({vcache_be_req, dcache_be_req}),
+// 	.m_resp ({vcache_be_resp, dcache_be_resp}),
+//  `else
+//         .m_req  (dcache_be_req),
+//         .m_resp (dcache_be_resp),
+//  `endif
+// `endif                 
+//         // slave
+//         .s_req  (l2cache_req),
+//         .s_resp (l2cache_resp)
+//         );
+
    merge
      #(
 `ifdef RUN_DDR_USE_SRAM
-     .N_MASTERS(2),
+       .N_MASTERS(2),
 `else
  `ifdef USE_NEW_VERSAT
-       .N_MASTERS(1+3),
+       .N_MASTERS(1+1),
  `else
        .N_MASTERS(1),
  `endif
 `endif
        .DATA_W(`MIG_BUS_W)
        )
-     merge_i_d_buses_into_l2
-       (
-        // masters
+   merge_i_d_buses_into_l2_read
+     (
+      // masters
 `ifdef RUN_DDR_USE_SRAM
-        .m_req  ({icache_be_req, dcache_be_req}),
-        .m_resp ({icache_be_resp, dcache_be_resp}),
+      .m_req  ({icache_be_req, dcache_be_req}),
+      .m_resp ({icache_be_resp, dcache_be_resp}),
 `else
  `ifdef USE_NEW_VERSAT
-	.m_req  ({vcache_be_req, dcache_be_req}),
-	.m_resp ({vcache_be_resp, dcache_be_resp}),
+      .m_req  ({vcache_be_req[`req_MIG_BUS(0)], dcache_read_be_req}),
+      .m_resp ({vcache_be_resp[`resp_MIG_BUS(0)], dcache_read_be_resp}),
  `else
-        .m_req  (dcache_be_req),
-        .m_resp (dcache_be_resp),
+      .m_req  (dcache_be_req),
+      .m_resp (dcache_be_resp),
  `endif
 `endif                 
-        // slave
-        .s_req  (l2cache_req),
-        .s_resp (l2cache_resp)
-        );
+      // slave
+      .s_req  (l2cache_req[`req_MIG_BUS(0)]),
+      .s_resp (l2cache_resp[`resp_MIG_BUS(0)])
+      );
 
-   // Connect Buses for L2 caches
+   merge
+     #(
+`ifdef RUN_DDR_USE_SRAM
+       .N_MASTERS(2),
+`else
+ `ifdef USE_NEW_VERSAT
+       .N_MASTERS(1+1),
+ `else
+       .N_MASTERS(1),
+ `endif
+`endif
+       .DATA_W(`MIG_BUS_W)
+       )
+   merge_i_d_buses_into_l2_write
+     (
+      // masters
+`ifdef RUN_DDR_USE_SRAM
+      .m_req  ({icache_be_req, dcache_be_req}),
+      .m_resp ({icache_be_resp, dcache_be_resp}),
+`else
+ `ifdef USE_NEW_VERSAT
+      .m_req  ({vcache_be_req[`req_MIG_BUS(1)], dcache_write_be_req}),
+      .m_resp ({vcache_be_resp[`resp_MIG_BUS(1)], dcache_write_be_resp}),
+ `else
+      .m_req  (dcache_be_req),
+      .m_resp (dcache_be_resp),
+ `endif
+`endif                 
+      // slave
+      .s_req  (l2cache_req[`req_MIG_BUS(1)]),
+      .s_resp (l2cache_resp[`resp_MIG_BUS(1)])
+      );
 
-   assign l2cache_read_valid = l2cache_req[`valid_MIG_BUS(0)] & (~(|l2cache_req[`wstrb_MIG_BUS(0)]));
-   assign l2cache_write_valid = l2cache_req[`valid_MIG_BUS(0)] & (|l2cache_req[`wstrb_MIG_BUS(0)]);
-   
-   assign l2cache_resp[`ready_MIG_BUS(0)] = l2cache_read_ready | l2cache_write_ready;
-   
-
+  
    // L2 cache instance
    iob_cache_axi # 
      (
       .FE_ADDR_W(`DDR_ADDR_W),
-      .N_WAYS(4),        //Number of Ways
-      .LINE_OFF_W(4),    //Cache Line Offset (number of lines)
+      .N_WAYS(1),        //Number of Ways
+      .LINE_OFF_W(1),    //Cache Line Offset (number of lines)
       .WORD_OFF_W(4),    //Word Offset (number of words per line)
-      .WTBUF_DEPTH_W(4), //FIFO's depth
+      .WTBUF_DEPTH_W(2), //FIFO's depth
       .BE_ADDR_W (`DDR_ADDR_W),
       .CTRL_CACHE (0),
       .CTRL_CNT(0),       //Remove counters
@@ -330,12 +512,12 @@ module ext_mem
             .reset (rst),
       
             // Native interface
-            .valid    (l2cache_read_valid),
+            .valid    (l2cache_req[`valid_MIG_BUS(0)]),
             .addr     (l2cache_req[`address_MIG_BUS(0,`DDR_ADDR_W+1)-$clog2(`MIG_BUS_W/8)]),
             .wdata    (l2cache_req[`wdata_MIG_BUS(0)]),
    	    .wstrb    (l2cache_req[`wstrb_MIG_BUS(0)]),
             .rdata    (l2cache_resp[`rdata_MIG_BUS(0)]),
-            .ready    (l2cache_read_ready),
+            .ready    (l2cache_resp[`ready_MIG_BUS(0)]),
 
 	    
             // AXI interface
@@ -388,9 +570,9 @@ module ext_mem
    iob_cache_axi # 
      (
       .FE_ADDR_W(`DDR_ADDR_W),
-      .N_WAYS(4),        //Number of Ways
-      .LINE_OFF_W(4),    //Cache Line Offset (number of lines)
-      .WORD_OFF_W(4),    //Word Offset (number of words per line)
+      .N_WAYS(1),        //Number of Ways
+      .LINE_OFF_W(1),    //Cache Line Offset (number of lines)
+      .WORD_OFF_W(1),    //Word Offset (number of words per line)
       .WTBUF_DEPTH_W(4), //FIFO's depth
       .BE_ADDR_W (`DDR_ADDR_W),
       .CTRL_CACHE (0),
@@ -403,12 +585,12 @@ module ext_mem
             .reset (rst),
       
             // Native interface
-            .valid    (l2cache_write_valid),
-            .addr     (l2cache_req[`address_MIG_BUS(0,`DDR_ADDR_W+1)-$clog2(`MIG_BUS_W/8)]),
-            .wdata    (l2cache_req[`wdata_MIG_BUS(0)]),
-   	    .wstrb    (l2cache_req[`wstrb_MIG_BUS(0)]),
+            .valid    (l2cache_req[`valid_MIG_BUS(1)]),
+            .addr     (l2cache_req[`address_MIG_BUS(1,`DDR_ADDR_W+1)-$clog2(`MIG_BUS_W/8)]),
+            .wdata    (l2cache_req[`wdata_MIG_BUS(1)]),
+   	    .wstrb    (l2cache_req[`wstrb_MIG_BUS(1)]),
             .rdata    (),
-            .ready    (l2cache_write_ready),
+            .ready    (l2cache_resp[`ready_MIG_BUS(1)]),
 
 	    
             // AXI interface
@@ -455,83 +637,5 @@ module ext_mem
             .axi_rvalid(1'b0),  
             .axi_rready()
             );
-
-
-
-
-
-   
-   // // L2 cache instance
-   // iob_cache_axi # 
-   //   (
-   //    .FE_ADDR_W(`DDR_ADDR_W),
-   //    .N_WAYS(4),        //Number of Ways
-   //    .LINE_OFF_W(4),    //Cache Line Offset (number of lines)
-   //    .WORD_OFF_W(4),    //Word Offset (number of words per line)
-   //    .WTBUF_DEPTH_W(4), //FIFO's depth
-   //    .BE_ADDR_W (`DDR_ADDR_W),
-   //    .CTRL_CACHE (0),
-   //    .CTRL_CNT(0),       //Remove counters
-   //    .FE_DATA_W(`MIG_BUS_W),
-   //    .BE_DATA_W(`MIG_BUS_W)
-   //    )
-   // l2cache (
-   //          .clk   (clk),
-   //          .reset (rst),
-      
-   //          // Native interface
-   //          .valid    (l2cache_req[`valid_MIG_BUS(0)]),
-   //          .addr     (l2cache_req[`address_MIG_BUS(0,`DDR_ADDR_W+1)-$clog2(`MIG_BUS_W/8)]),
-   //          .wdata    (l2cache_req[`wdata_MIG_BUS(0)]),
-   // 	    .wstrb    (l2cache_req[`wstrb_MIG_BUS(0)]),
-   //          .rdata    (l2cache_resp[`rdata_MIG_BUS(0)]),
-   //          .ready    (l2cache_resp[`ready_MIG_BUS(0)]),
-
-	    
-   //          // AXI interface
-   //          // Address write
-   //          .axi_awid(axi_awid), 
-   //          .axi_awaddr(axi_awaddr), 
-   //          .axi_awlen(axi_awlen), 
-   //          .axi_awsize(axi_awsize), 
-   //          .axi_awburst(axi_awburst), 
-   //          .axi_awlock(axi_awlock), 
-   //          .axi_awcache(axi_awcache), 
-   //          .axi_awprot(axi_awprot),
-   //          .axi_awqos(axi_awqos), 
-   //          .axi_awvalid(axi_awvalid), 
-   //          .axi_awready(axi_awready), 
-   //          //write
-   //          .axi_wdata(axi_wdata), 
-   //          .axi_wstrb(axi_wstrb), 
-   //          .axi_wlast(axi_wlast), 
-   //          .axi_wvalid(axi_wvalid), 
-   //          .axi_wready(axi_wready), 
-   //          //write response
-   //          //.axi_bid(axi_bid), 
-   //          .axi_bresp(axi_bresp), 
-   //          .axi_bvalid(axi_bvalid), 
-   //          .axi_bready(axi_bready), 
-   //          //address read
-   //          .axi_arid(axi_arid), 
-   //          .axi_araddr(axi_araddr), 
-   //          .axi_arlen(axi_arlen), 
-   //          .axi_arsize(axi_arsize), 
-   //          .axi_arburst(axi_arburst), 
-   //          .axi_arlock(axi_arlock), 
-   //          .axi_arcache(axi_arcache), 
-   //          .axi_arprot(axi_arprot), 
-   //          .axi_arqos(axi_arqos), 
-   //          .axi_arvalid(axi_arvalid), 
-   //          .axi_arready(axi_arready), 
-   //          //read 
-   //          //.axi_rid(axi_rid), 
-   //          .axi_rdata(axi_rdata), 
-   //          .axi_rresp(axi_rresp), 
-   //          .axi_rlast(axi_rlast), 
-   //          .axi_rvalid(axi_rvalid),  
-   //          .axi_rready(axi_rready)
-   //          );
-
 endmodule
 
