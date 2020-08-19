46805a2f6201da3e68b3fafe8b7f2a3f2212acff
P_Miranda <pedro_miranda2@hotmail.com>
2020-08-19 17:55:18 +0100

    Only add axi_interconnect as source for simulation

--- hardware/simulation/simulation.mk
+++ hardware/simulation/simulation.mk
@@ -7,4 +7,4 @@ DEFINE+=$(defmacro)VCD
 endif
 
 #testbench source files
-VSRC+=$(TB_DIR)/$(TEST)_tb.v $(AXI_MEM_DIR)/rtl/axi_ram.v
+VSRC+=$(TB_DIR)/$(TEST)_tb.v $(AXI_MEM_DIR)/rtl/axi_ram.v $(AXI_MEM_DIR)/rtl/arbiter.v $(AXI_MEM_DIR)/rtl/priority_encoder.v $(AXI_MEM_DIR)/rtl/axi_interconnect.v
