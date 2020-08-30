f97bc22eca1131c16a262ea2dd8d82bdc523ef37
dgarigali <daniel.garigali@gmail.com>
2020-08-30 17:20:00 +0100

    Cleaned up a bit sw code

--- software/yolo_hw/firmware.h
+++ software/yolo_hw/firmware.h
@@ -1,9 +1,4 @@
 //Layer1,2 (conv + maxpool)
-#define LAYER_1_INPADD 1
-#define LAYER_1_IGNOREPAD 0
-#define LAYER_1_STRIDE 0
-#define LAYER_1_OUTPADD 1
-#define LAYER_1_MAXPOOL 1
 #define LAYER_1_W 416
 #define LAYER_1_NUM_KER 16
 #define LAYER_1_KER_SIZE 3
@@ -16,10 +11,6 @@
 #define DATA_LAYER_2 (LAYER_2_W*LAYER_2_W*LAYER_1_NUM_KER)
 
 //Layer3,4 (conv + maxpool)
-#define LAYER_3_IGNOREPAD 0
-#define LAYER_3_STRIDE 0
-#define LAYER_3_OUTPADD 1
-#define LAYER_3_MAXPOOL 1
 #define LAYER_3_W 208
 #define LAYER_3_NUM_KER 32
 #define LAYER_3_KER_SIZE 3
@@ -28,10 +19,6 @@
 #define DATA_LAYER_4 (LAYER_4_W*LAYER_4_W*LAYER_3_NUM_KER)
 
 //Layer5,6 (conv + maxpool)
-#define LAYER_5_IGNOREPAD 0
-#define LAYER_5_STRIDE 0
-#define LAYER_5_OUTPADD 1
-#define LAYER_5_MAXPOOL 1
 #define LAYER_5_W 104
 #define LAYER_5_NUM_KER 64
 #define LAYER_5_KER_SIZE 3
@@ -40,10 +27,7 @@
 #define DATA_LAYER_6 (LAYER_6_W*LAYER_6_W*LAYER_5_NUM_KER)
 
 //Layer7,8 (conv + maxpool)
-#define LAYER_7_IGNOREPAD 0
-#define LAYER_7_STRIDE 0
 #define LAYER_7_OUTPADD 1
-#define LAYER_7_MAXPOOL 1
 #define LAYER_7_W 52
 #define LAYER_7_NUM_KER 128
 #define LAYER_7_KER_SIZE 3
@@ -52,10 +36,13 @@
 #define DATA_LAYER_8 (LAYER_8_W*LAYER_8_W*LAYER_7_NUM_KER)
 
 //Layer9 (conv)
+#define LAYER_9_PINGPONG 1
+#define LAYER_9_ZXY 1
+#define LAYER_9_LEAKY 1
+#define LAYER_9_UPSAMPLE 0
 #define LAYER_9_IGNOREPAD 0
 #define LAYER_9_STRIDE 0
 #define LAYER_9_OUTPADD 1
-#define LAYER_9_MAXPOOL 0
 #define LAYER_9_W 26
 #define LAYER_9_NUM_KER 256
 #define LAYER_9_KER_SIZE 3
@@ -63,16 +50,20 @@
 #define DATA_LAYER_9 ((LAYER_9_W+2)*(LAYER_9_W+2)*LAYER_9_NUM_KER)
 
 //Layer10 (maxpool)
+#define LAYER_10_OUTPADD 1
 #define LAYER_10_INPADD 1
 #define LAYER_10_STRIDE 0
 #define LAYER_10_W 15
 #define DATA_LAYER_10 (LAYER_10_W*LAYER_10_W*LAYER_9_NUM_KER)
 
 //Layer11 (conv)
+#define LAYER_11_PINGPONG 0
+#define LAYER_11_ZXY 1
+#define LAYER_11_LEAKY 1
+#define LAYER_11_UPSAMPLE 0
 #define LAYER_11_IGNOREPAD 0
 #define LAYER_11_STRIDE 1
 #define LAYER_11_OUTPADD 0
-#define LAYER_11_MAXPOOL 0
 #define LAYER_11_W 13
 #define LAYER_11_NUM_KER 512
 #define LAYER_11_KER_SIZE 3
@@ -80,16 +71,20 @@
 #define DATA_LAYER_11 ((LAYER_11_W+1)*(LAYER_11_W+1)*LAYER_11_NUM_KER)
 
 //Layer12 (maxpool)
+#define LAYER_12_OUTPADD 1
 #define LAYER_12_INPADD 0
 #define LAYER_12_STRIDE 1
 #define LAYER_12_W 15
 #define DATA_LAYER_12 (LAYER_12_W*LAYER_12_W*LAYER_11_NUM_KER)
 
 //Layer13 (conv)
+#define LAYER_13_PINGPONG 0
+#define LAYER_13_ZXY 1
+#define LAYER_13_LEAKY 1
+#define LAYER_13_UPSAMPLE 0
 #define LAYER_13_IGNOREPAD 0
 #define LAYER_13_STRIDE 0
 #define LAYER_13_OUTPADD 0
-#define LAYER_13_MAXPOOL 0
 #define LAYER_13_W 13
 #define LAYER_13_NUM_KER 1024
 #define LAYER_13_KER_SIZE 3
@@ -97,10 +92,13 @@
 #define DATA_LAYER_13 (LAYER_13_W*LAYER_13_W*LAYER_13_NUM_KER)
 
 //Layer14 (conv)
+#define LAYER_14_PINGPONG 0
+#define LAYER_14_ZXY 0
+#define LAYER_14_LEAKY 1
+#define LAYER_14_UPSAMPLE 0
 #define LAYER_14_IGNOREPAD 0
 #define LAYER_14_STRIDE 0
 #define LAYER_14_OUTPADD 1
-#define LAYER_14_MAXPOOL 0
 #define LAYER_14_W 13
 #define LAYER_14_NUM_KER 256
 #define LAYER_14_KER_SIZE 1
@@ -108,10 +106,13 @@
 #define DATA_LAYER_14 ((LAYER_14_W+2)*(LAYER_14_W+2)*LAYER_14_NUM_KER)
 
 //Layer15 (conv)
+#define LAYER_15_PINGPONG 0
+#define LAYER_15_ZXY 0
+#define LAYER_15_LEAKY 1
+#define LAYER_15_UPSAMPLE 0
 #define LAYER_15_IGNOREPAD 0
 #define LAYER_15_STRIDE 0
 #define LAYER_15_OUTPADD 0
-#define LAYER_15_MAXPOOL 0
 #define LAYER_15_W 13
 #define LAYER_15_NUM_KER 512
 #define LAYER_15_KER_SIZE 3
@@ -119,10 +120,13 @@
 #define DATA_LAYER_15 (LAYER_15_W*LAYER_15_W*LAYER_15_NUM_KER)
 
 //Layer16 (conv + yolo)
+#define LAYER_16_PINGPONG 0
+#define LAYER_16_ZXY 0
+#define LAYER_16_LEAKY 0
+#define LAYER_16_UPSAMPLE 0
 #define LAYER_16_IGNOREPAD 0
 #define LAYER_16_STRIDE 0
 #define LAYER_16_OUTPADD 0
-#define LAYER_16_MAXPOOL 0
 #define LAYER_16_W 13
 #define LAYER_16_NUM_KER 256
 #define LAYER_16_KER_SIZE 1
@@ -130,10 +134,13 @@
 #define DATA_LAYER_16 (LAYER_16_W*LAYER_16_W*LAYER_16_NUM_KER)
 
 //Layer19 (conv + upsample)
+#define LAYER_19_PINGPONG 1
+#define LAYER_19_ZXY 0
+#define LAYER_19_LEAKY 1
+#define LAYER_19_UPSAMPLE 1
 #define LAYER_19_IGNOREPAD 1
 #define LAYER_19_STRIDE 0
 #define LAYER_19_OUTPADD 1
-#define LAYER_19_MAXPOOL 0
 #define LAYER_19_W 13
 #define LAYER_19_NUM_KER 128
 #define LAYER_19_KER_SIZE 1
@@ -141,10 +148,13 @@
 #define DATA_LAYER_19 ((LAYER_19_W*2+2)*(LAYER_19_W*2+2)*LAYER_19_NUM_KER)
 
 //Layer22 (conv)
+#define LAYER_22_PINGPONG 0
+#define LAYER_22_ZXY 0
+#define LAYER_22_LEAKY 1
+#define LAYER_22_UPSAMPLE 0
 #define LAYER_22_IGNOREPAD 0
 #define LAYER_22_STRIDE 0
 #define LAYER_22_OUTPADD 0
-#define LAYER_22_MAXPOOL 0
 #define LAYER_22_W 26
 #define LAYER_22_NUM_KER 256
 #define LAYER_22_KER_SIZE 3
@@ -152,10 +162,13 @@
 #define DATA_LAYER_22 (LAYER_22_W*LAYER_22_W*LAYER_22_NUM_KER)
 
 //Layer23 (conv + yolo)
+#define LAYER_23_PINGPONG 1
+#define LAYER_23_ZXY 0
+#define LAYER_23_LEAKY 0
+#define LAYER_23_UPSAMPLE 0
 #define LAYER_23_IGNOREPAD 0
 #define LAYER_23_STRIDE 0
 #define LAYER_23_OUTPADD 0
-#define LAYER_23_MAXPOOL 0
 #define LAYER_23_W 26
 #define LAYER_23_NUM_KER 256
 #define LAYER_23_KER_SIZE 1
