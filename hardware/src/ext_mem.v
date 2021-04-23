216a47fd46575b48fa32918988c5d40ec8df7062
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-04-23 10:48:25 +0100

    fix ext_mem module when RUN_DDR flag is active

--- hardware/src/ext_mem.v
+++ hardware/src/ext_mem.v
@@ -7,7 +7,7 @@
 `endif
 
 // REQ/RESP MIG BUS DEFINES
-`define REQ_MIG_BUS_W (`VALID_W+`DCACHE_ADDR_W+`MIG_BUS_W+(`MIG_BUS_W/8))
+`define REQ_MIG_BUS_W (`VALID_W+ADDR_W+`MIG_BUS_W+(`MIG_BUS_W/8))
 `define RESP_MIG_BUS_W (`MIG_BUS_W+`READY_W)
 `define WDATA_MIG_BUS_P (`MIG_BUS_W/8)
 `define ADDR_MIG_BUS_P (`WDATA_MIG_BUS_P + `MIG_BUS_W)
@@ -26,59 +26,59 @@ module ext_mem
     parameter DATA_W=`DATA_W
     )
    (
-    input                                    clk,
-    input                                    rst,
+    input                                     clk,
+    input                                     rst,
 
 `ifdef RUN_DDR_USE_SRAM
     // Instruction bus
-    input [1+`FIRM_ADDR_W-2+`WRITE_W-1:0]    i_req,
-    output [`RESP_W-1:0]                     i_resp,
+    input [1+`FIRM_ADDR_W-2+`WRITE_W-1:0]     i_req,
+    output [`RESP_W-1:0]                      i_resp,
 `endif
 
     // Data bus
     input [1+1+`DCACHE_ADDR_W-2+`WRITE_W-1:0] d_req,
-    output [`RESP_W-1:0]                     d_resp,
+    output [`RESP_W-1:0]                      d_resp,
 
     // AXI interface 
     // Address write
-    output [0:0]                             axi_awid, 
-    output [`DDR_ADDR_W-1:0]                 axi_awaddr,
-    output [7:0]                             axi_awlen,
-    output [2:0]                             axi_awsize,
-    output [1:0]                             axi_awburst,
-    output [0:0]                             axi_awlock,
-    output [3:0]                             axi_awcache,
-    output [2:0]                             axi_awprot,
-    output [3:0]                             axi_awqos,
-    output                                   axi_awvalid,
-    input                                    axi_awready,
+    output [0:0]                              axi_awid, 
+    output [`DDR_ADDR_W-1:0]                  axi_awaddr,
+    output [7:0]                              axi_awlen,
+    output [2:0]                              axi_awsize,
+    output [1:0]                              axi_awburst,
+    output [0:0]                              axi_awlock,
+    output [3:0]                              axi_awcache,
+    output [2:0]                              axi_awprot,
+    output [3:0]                              axi_awqos,
+    output                                    axi_awvalid,
+    input                                     axi_awready,
     //Write
-    output [`MIG_BUS_W-1:0]                  axi_wdata,
-    output [`MIG_BUS_W/8-1:0]                axi_wstrb,
-    output                                   axi_wlast,
-    output                                   axi_wvalid, 
-    input                                    axi_wready,
-    input [1:0]                              axi_bresp,
-    input                                    axi_bvalid,
-    output                                   axi_bready,
+    output [`MIG_BUS_W-1:0]                   axi_wdata,
+    output [`MIG_BUS_W/8-1:0]                 axi_wstrb,
+    output                                    axi_wlast,
+    output                                    axi_wvalid, 
+    input                                     axi_wready,
+    input [1:0]                               axi_bresp,
+    input                                     axi_bvalid,
+    output                                    axi_bready,
     //Address Read
-    output [0:0]                             axi_arid,
-    output [`DDR_ADDR_W-1:0]                 axi_araddr, 
-    output [7:0]                             axi_arlen,
-    output [2:0]                             axi_arsize,
-    output [1:0]                             axi_arburst,
-    output [0:0]                             axi_arlock,
-    output [3:0]                             axi_arcache,
-    output [2:0]                             axi_arprot,
-    output [3:0]                             axi_arqos,
-    output                                   axi_arvalid, 
-    input                                    axi_arready,
+    output [0:0]                              axi_arid,
+    output [`DDR_ADDR_W-1:0]                  axi_araddr, 
+    output [7:0]                              axi_arlen,
+    output [2:0]                              axi_arsize,
+    output [1:0]                              axi_arburst,
+    output [0:0]                              axi_arlock,
+    output [3:0]                              axi_arcache,
+    output [2:0]                              axi_arprot,
+    output [3:0]                              axi_arqos,
+    output                                    axi_arvalid, 
+    input                                     axi_arready,
     //Read
-    input [`MIG_BUS_W-1:0]                      axi_rdata,
-    input [1:0]                              axi_rresp,
-    input                                    axi_rlast, 
-    input                                    axi_rvalid, 
-    output                                   axi_rready
+    input [`MIG_BUS_W-1:0]                    axi_rdata,
+    input [1:0]                               axi_rresp,
+    input                                     axi_rlast, 
+    input                                     axi_rvalid, 
+    output                                    axi_rready
     );
 
 `ifdef RUN_DDR_USE_SRAM
@@ -87,8 +87,8 @@ module ext_mem
    //
 
    // Back-end bus
-   wire [`REQ_MIG_BUS_W-1:0]      icache_be_req;
-   wire [`RESP_MIG_BUS_W-1:0]     icache_be_resp;
+   wire [1+`DCACHE_ADDR_W+`MIG_BUS_W+(`MIG_BUS_W/8)-1:0] icache_be_req;
+   wire [`RESP_MIG_BUS_W-1:0]                            icache_be_resp;
 
 
    // Instruction cache instance
@@ -121,7 +121,7 @@ module ext_mem
            .wtb_empty_in(1'b1),
            .wtb_empty_out(),
            // Back-end interface
-           .mem_valid (icache_be_req[`valid_MIG_BUS(0)]),
+           .mem_valid (icache_be_req[1+`DCACHE_ADDR_W+`MIG_BUS_W+(`MIG_BUS_W/8)-1]),
            .mem_addr  (icache_be_req[`address_MIG_BUS(0, `DCACHE_ADDR_W)]),
            .mem_wdata (icache_be_req[`wdata_MIG_BUS(0)]),
            .mem_wstrb (icache_be_req[`wstrb_MIG_BUS(0)]),
@@ -131,8 +131,8 @@ module ext_mem
 `endif //  `ifdef RUN_DDR_USE_SRAM
 
    //l2 cache interface signals
-   wire [`REQ_MIG_BUS_W-1:0]                 l2cache_req;
-   wire [`RESP_MIG_BUS_W-1:0]                l2cache_resp;
+   wire [1+`DCACHE_ADDR_W+`MIG_BUS_W+(`MIG_BUS_W/8)-1:0] l2cache_req;
+   wire [`RESP_MIG_BUS_W-1:0]                            l2cache_resp;
    
    //ext_mem control signals
    wire                                      l2_wtb_empty;
@@ -157,7 +157,7 @@ module ext_mem
    //
 
    // Back-end bus
-   wire [`REQ_MIG_BUS_W-1:0]      dcache_be_req;
+   wire [1+`DCACHE_ADDR_W+`MIG_BUS_W+(`MIG_BUS_W/8)-1:0]      dcache_be_req;
    wire [`RESP_MIG_BUS_W-1:0]     dcache_be_resp;
    
    // Data cache instance
@@ -189,7 +189,7 @@ module ext_mem
            .wtb_empty_in(l2_wtb_empty),
            .wtb_empty_out(),
            // Back-end interface
-           .mem_valid (dcache_be_req[`valid_MIG_BUS(0)]),
+           .mem_valid (dcache_be_req[1+`DCACHE_ADDR_W+`MIG_BUS_W+(`MIG_BUS_W/8)-1]),
            .mem_addr  (dcache_be_req[`address_MIG_BUS(0,`DCACHE_ADDR_W)]),
            .mem_wdata (dcache_be_req[`wdata_MIG_BUS(0)]),
            .mem_wstrb (dcache_be_req[`wstrb_MIG_BUS(0)]),
@@ -247,12 +247,21 @@ module ext_mem
             .reset (rst),
       
             // Native interface
-            .valid    (l2cache_req[`valid_MIG_BUS(0)]),
+`ifdef RUN_DDR_USE_SRAM
+            .valid    (l2cache_req[1+`DCACHE_ADDR_W+`MIG_BUS_W+(`MIG_BUS_W/8)-1]),
             .addr     (l2cache_req[`address_MIG_BUS(0, `DCACHE_ADDR_W)-$clog2(`MIG_BUS_W/8)]),
             .wdata    (l2cache_req[`wdata_MIG_BUS(0)]),
             .wstrb    (l2cache_req[`wstrb_MIG_BUS(0)]),
             .rdata    (l2cache_resp[`rdata_MIG_BUS(0)]),
             .ready    (l2cache_resp[`ready_MIG_BUS(0)]),
+`else
+            .valid    (d_req[`valid(0)]),
+            .addr     (d_req[`address(0,`DDR_ADDR_W)-$clog2(`ADDR_W/8)]),
+            .wdata    (d_req[`wdata(0)]),
+            .wstrb    (d_req[`wstrb(0)]),
+            .rdata    (d_resp[`rdata(0)]),
+            .ready    (d_resp[`ready(0)]),
+`endif
             //Control IO
             .force_inv_in(invalidate_reg & ~l2_valid),
             .force_inv_out(),
